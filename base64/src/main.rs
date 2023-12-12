72e7c8e0869b83b88a44f2821cb3fe58433d8c45
Owen Krueger <github@owenkrueger.dev>
2023-12-12 16:45:26 -0600

    Add tests and build flow

--- base64/src/main.rs
+++ base64/src/main.rs
@@ -26,16 +26,59 @@ fn main() {
         return;
     }
 
-    if args.encode {
-        println!("{}", general_purpose::STANDARD.encode(args.string));
-    } else if args.decode {
-        match general_purpose::STANDARD.decode(args.string) {
+    println!("{}", get_string(args.string, args.encode));
+}
+
+/// Base64 encodes or decodes the input string.
+///
+/// # Arguments
+///
+/// * `input`: String to encode/decode.
+/// * `encode`: If true, the string will be encoded. If false, the string will be decoded.
+///
+/// returns: String The encoded/decoded string.
+///
+/// # Examples
+///
+/// ```
+/// let encoded_string = get_string(String::from("Hello World!"), true); // SGVsbG8gV29ybGQh
+///
+/// let decoded_string = get_string(String::from("SGVsbG8gV29ybGQh"), false); // Hello World!
+/// ```
+fn get_string(input: String, encode: bool) -> String {
+    return if encode {
+        general_purpose::STANDARD.encode(input)
+    } else {
+        match general_purpose::STANDARD.decode(input) {
             Ok(r) => {
-                println!("{}", String::from_utf8(r).unwrap());
+                String::from_utf8(r).unwrap()
             }
             Err(e) => {
-                println!("{}", e);
+                e.to_string()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode() {
+        let input = String::from("Hello World!");
+        let expected = String::from("SGVsbG8gV29ybGQh");
+        let actual = get_string(input, true);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_decode() {
+        let input = String::from("SGVsbG8gV29ybGQh");
+        let expected = String::from("Hello World!");
+        let actual = get_string(input, false);
+
+        assert_eq!(expected, actual);
     }
 }
--- guid/src/main.rs
+++ guid/src/main.rs
@@ -13,11 +13,51 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let mut guid = String::from("00000000-0000-0000-0000-000000000000");
 
-    if !args.empty {
-        guid = GUID::rand().to_string();
+    println!("{}", get_guid(args.empty));
+}
+
+
+/// Generates a guid.
+///
+/// # Arguments
+///
+/// * `empty`: Whether or not the guid should be empty.
+///
+/// returns: String The generated guid.
+///
+/// # Examples
+///
+/// ```
+/// let non_empty_guid = get_guid(false); // Guid is not 00000000-0000-0000-0000-000000000000.
+///
+/// let empty_guid = get_guid(true); // Guid is 00000000-0000-0000-0000-000000000000.
+/// ```
+fn get_guid(empty: bool) -> String {
+    return if empty {
+        String::from("00000000-0000-0000-0000-000000000000")
+    } else {
+        GUID::rand().to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let expected = String::from("00000000-0000-0000-0000-000000000000");
+        let actual = get_guid(true);
+
+        assert_eq!(expected, actual);
     }
 
-    println!("{}", guid);
+    #[test]
+    fn test_guid() {
+        let expected = String::from("00000000-0000-0000-0000-000000000000");
+        let actual = get_guid(false);
+
+        assert_ne!(expected, actual);
+    }
 }
