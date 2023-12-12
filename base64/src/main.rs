141ff64497e82b81f8475036d23704ecdfe06641
Owen Krueger <github@owenkrueger.dev>
2023-12-12 16:31:36 -0600

    Stop copying to clipboard

--- base64/src/main.rs
+++ base64/src/main.rs
@@ -1,10 +1,9 @@
-use arboard::Clipboard;
 use base64::{Engine as _, engine::general_purpose};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
-/// Encodes or Decodes the input string. Must use either 'encode' or 'decode' flag. String copied to clipboard.
+/// Encodes or Decodes the input string. Must use either 'encode' or 'decode' flag.
 struct Args {
 
     /// String to encode/decode.
@@ -23,26 +22,20 @@ fn main() {
     let args = Args::parse();
 
     if !args.encode && !args.decode {
-        println!("Must use either --encode or --decode flag.");
+        eprintln!("Must use either --encode or --decode flag.");
         return;
     }
 
     if args.encode {
-        save_to_clipboard(&general_purpose::STANDARD.encode(args.string));
+        println!("{}", general_purpose::STANDARD.encode(args.string));
     } else if args.decode {
         match general_purpose::STANDARD.decode(args.string) {
             Ok(r) => {
-                save_to_clipboard(&String::from_utf8(r).unwrap());
+                println!("{}", String::from_utf8(r).unwrap());
             }
             Err(e) => {
                 println!("{}", e);
             }
         }
     }
-}
-
-fn save_to_clipboard(string: &str) {
-    let mut clipboard = Clipboard::new().unwrap();
-    clipboard.set_text(string).unwrap();
-    println!("{} copied to clipboard", string);
 }
--- guid/src/main.rs
+++ guid/src/main.rs
@@ -1,10 +1,9 @@
-use arboard::Clipboard;
 use clap::Parser;
 use guid_create::GUID;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
-/// Generates a guid. Guid copied to clipboard.
+/// Generates a guid.
 struct Args {
 
     /// Generates an empty guid (00000000-0000-0000-0000-000000000000).
@@ -14,13 +13,11 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let mut clipboard = Clipboard::new().unwrap();
     let mut guid = String::from("00000000-0000-0000-0000-000000000000");
 
     if !args.empty {
         guid = GUID::rand().to_string();
     }
 
-    clipboard.set_text(&guid).unwrap();
-    println!("{} copied to clipboard.", guid);
+    println!("{}", guid);
 }
